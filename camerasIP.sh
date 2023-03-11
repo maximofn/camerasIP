5c29ec02942fb70832cf7b1460bdfa58ece34734
maximofn <maximofn@gmail.com>
2023-03-12 00:57:16 +0100

    Se modifica el .sh para que el .py se ejecute en segundo plano y no se quede bloqueada la terminal

--- camerasIP.sh
+++ camerasIP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Cameras IP"
 source ~/miniconda3/bin/activate camerasIP  # Change to camerasIP conda environment
-python /home/wallabot/Documentos/camerasIP/camerasIP.py
+python /home/wallabot/Documentos/camerasIP/camerasIP.py & # Run camerasIP.py script in background
